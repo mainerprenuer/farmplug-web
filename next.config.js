fd4151ffb29206ee3592e91f7e1be9c7c8070cb4
mainerprenuer <moustymainer@gmail.com>
2023-08-06 09:04:51 +0100

    setting up proxy on the next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    reactStrictMode: true,
+    swcMinifyMode: true, 
+    experimental: {
+        esmExternals: true,
+    },
+}
 
 module.exports = nextConfig

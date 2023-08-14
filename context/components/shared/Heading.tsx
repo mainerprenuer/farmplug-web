96e49abf84f5bb0526803b5dfaa4e4bc1a118772
mainerprenuer <moustymainer@gmail.com>
2023-08-14 23:16:34 +0100

    added head & toast provider.

---
+++ context/components/shared/Heading.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head"
+
+const Heading = () => {
+  return (
+    <Head>
+        <title>Farmplug</title>
+        <meta name="description" content="Farmplug - Get Fresh Fruits in a Click" />
+        <link rel="icon" href="/favicon.ico" />
+    </Head>
+  );
+};
+
+export default Heading;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,19 @@
 import '../styles/global.css';
 import type { AppProps } from 'next/app';
 import { Provider } from '../context';
+import Heading from '../context/components/shared/Heading';
+import { Container } from 'react-bootstrap';
+import { ToastProvider } from 'react-toast-notifications';
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
         <Provider>
-            <Component {...pageProps}/>
+            <Heading/>
+            <Container>
+              <ToastProvider>
+                <Component {...pageProps} />
+              </ToastProvider>
+            </Container>
         </Provider>
     )
 }
